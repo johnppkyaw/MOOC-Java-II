6a040cca497ab93014c82d86f088978a85f44881
John Kyaw <jjpyae@gmail.com>
2024-01-13 17:39:58 -0800

    8.1 complete

--- part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,13 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String nextInput = scanner.nextLine();
+        while(!nextInput.equals("end")) {
+            int currNum = Integer.parseInt(nextInput);
+            int cube = currNum * currNum * currNum;
+            System.out.println(cube);
+            nextInput = scanner.nextLine();
+        }
+        scanner.close();
     }
 }
--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,10 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = scanner.nextInt();
+        int count = 1;
+        while()
+
 
     }
 }
